92a483b29bc6f3bee94fc3889acc763bf499dfa5
Honlan <493722771@qq.com>
2015-06-04 19:32:18 +0800

    team

--- wp-content/themes/accesspress-parallax/content-team.php
+++ wp-content/themes/accesspress-parallax/content-team.php
@@ -19,7 +19,24 @@ $post_date_class = ($post_date != 1 || has_post_thumbnail()) ? " no-date" : "";
 
 	<header class="entry-header">
 		<h1 class="entry-title<?php echo $post_date_class; ?>"><a href="<?php echo home_url()?>/team/#post-<?php the_ID(); ?>"><?php the_title(); ?></a></h1>
-		<h5><?php echo get_post_meta(get_the_ID(), "wpcf-degree", true);?></h5>	
+		<h5>
+			<?php 
+			switch(get_post_meta(get_the_ID(), "wpcf-degree", true)){
+				case 0:
+					echo "Professor";
+					break;
+				case 1:
+					echo "PhD Cand.";
+					break;
+				case 2:
+					echo "Master Cand.";
+					break;
+				case 3:
+					echo "Undergraduate";
+					break;
+			}
+			?>
+		</h5>	
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -35,17 +52,17 @@ $post_date_class = ($post_date != 1 || has_post_thumbnail()) ? " no-date" : "";
 			) );
 		?>
 		<div class="personal-extra">
-			<p><i class="fa fa-home fa-lg"></i>&nbsp;&nbsp;&nbsp;<a class="Website" href="<?php echo get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true);?>"><?php echo get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true);?></a></p>
-			<p><i class="fa fa-envelope fa-lg"></i>&nbsp;&nbsp;&nbsp;<a class="Email" href="mailto:<?php echo get_post_meta(get_the_ID(), 'wpcf-e-mail', true);?>"><?php echo get_post_meta(get_the_ID(), 'wpcf-e-mail', true);?></a></p>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true)!=''){?><p><i class="fa fa-home fa-lg"></i>&nbsp;&nbsp;&nbsp;<a class="Website" href="<?php echo get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true);?>"><?php echo get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true);?></a></p><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-e-mail', true)!=''){?><p><i class="fa fa-envelope fa-lg"></i>&nbsp;&nbsp;&nbsp;<a class="Email" href="mailto:<?php echo get_post_meta(get_the_ID(), 'wpcf-e-mail', true);?>"><?php echo get_post_meta(get_the_ID(), 'wpcf-e-mail', true);?></a></p><?php }?>
 
-			<a title="Website" href="<?php echo get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true);?>"><i class="fa fa-home fa-lg"></i></a>
-			<a title="Github" href="https://github.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-github', true);?>"><i class="fa fa-github fa-lg"></i></a>
-			<a title="Twitter" href="https://twitter.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-twitter', true);?>"><i class="fa fa-twitter fa-lg"></i></a>
-			<a title="Wechat" href="https://wx.qq.com/#<?php echo get_post_meta(get_the_ID(), 'wpcf-wechat', true);?>"><i class="fa fa-weixin fa-lg"></i></a>
-			<a title="Weibo" href="http://weibo.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-weibo', true);?>"><i class="fa fa-weibo fa-lg"></i></a>
-			<a title="Google+" href="http://plus.google.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-google', true);?>"><i class="fa fa-google-plus fa-lg"></i></a>
-			<a title="Linkedin" href="http://www.linkedin.com/in/<?php echo get_post_meta(get_the_ID(), 'wpcf-linkedin', true);?>"><i class="fa fa-linkedin-square fa-lg"></i></a>
-			<a title="Email" href="mailto:<?php echo get_post_meta(get_the_ID(), 'wpcf-e-mail', true);?>"><i class="fa fa-paper-plane-o fa-lg"></i></a>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true)!=''){?><a title="Website" href="<?php echo get_post_meta(get_the_ID(), 'wpcf-personal-homepage', true);?>"><i class="fa fa-home fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-github', true)!=''){?><a title="Github" href="https://github.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-github', true);?>"><i class="fa fa-github fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-twitter', true)!=''){?><a title="Twitter" href="https://twitter.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-twitter', true);?>"><i class="fa fa-twitter fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-wechat', true)!=''){?><a title="Wechat" href="https://wx.qq.com/#<?php echo get_post_meta(get_the_ID(), 'wpcf-wechat', true);?>"><i class="fa fa-weixin fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-weibo', true)!=''){?><a title="Weibo" href="http://weibo.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-weibo', true);?>"><i class="fa fa-weibo fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-google', true)!=''){?><a title="Google+" href="http://plus.google.com/<?php echo get_post_meta(get_the_ID(), 'wpcf-google', true);?>"><i class="fa fa-google-plus fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-linkedin', true)!=''){?><a title="Linkedin" href="http://www.linkedin.com/in/<?php echo get_post_meta(get_the_ID(), 'wpcf-linkedin', true);?>"><i class="fa fa-linkedin-square fa-lg"></i></a><?php }?>
+			<?php if(get_post_meta(get_the_ID(), 'wpcf-e-mail', true)!=''){?><a title="Email" href="mailto:<?php echo get_post_meta(get_the_ID(), 'wpcf-e-mail', true);?>"><i class="fa fa-paper-plane-o fa-lg"></i></a><?php }?>
 
 			<!-- <span class="tele"><?php echo get_post_meta(get_the_ID(), "wpcf-telephone-number", true);?></span> -->
 			<!-- <span class="gravatar"><?php echo get_post_meta(get_the_ID(), "wpcf-gravatar", true);?></span> -->
